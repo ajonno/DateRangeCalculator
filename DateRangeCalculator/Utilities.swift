b5e6c927def76803cebfbd1438b851fa04d4e7bc
Angus Johnston <ajonno68@gmail.com>
2016-01-20 21:15:01 +1100

    added logo to splash screen

---
+++ DateRangeCalculator/Utilities.swift
@@ -0,0 +1,29 @@
+//
+//  Utilities.swift
+//  DateRangeCalculator
+//
+//  Created by Angus Johnston on 20/01/2016.
+//  Copyright © 2016 Angus Johnston. All rights reserved.
+//
+
+import UIKit
+
+enum Month : Int {
+    case Jan = 1
+    case Feb = 2
+    case Mar = 3
+    case Apr = 4
+    case May = 5
+    case Jun = 6
+    case Jul = 7
+    case Aug = 8
+    case Sep = 9
+    case Oct = 10
+    case Nov = 11
+    case Dec = 12
+    
+    static let allValues = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
+    
+}
+
+let screenBounds = UIScreen.mainScreen().bounds
